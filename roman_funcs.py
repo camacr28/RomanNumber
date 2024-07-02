032e746afabcab3bb0dcdf66eff89a431f9ec3da
Carlos Mata <camacr28@gmail.com>
2024-07-02 21:30:34 +0200

    Conversion a romanos completa a falta de refactorizacion

--- roman_funcs.py
+++ roman_funcs.py
@@ -1,4 +1,4 @@
-valors = {
+values = {
     1: 'I',
     5: 'V',
     10: 'X',
@@ -11,23 +11,58 @@ valors = {
 
 def to_roman(n):
 
-    if n in valors:
-        result = valors[n]
+    if n in values:
+        result = values[n]
     elif n <= 3:
-        result = n * valors[1]
+        result = n * values[1]
     elif n == 4:
-        result = valors[1] + valors[5]
+        result = values[1] + values[5]
     elif n < 9:
-        result = valors[5] + (n - 5) * valors[1]
+        result = values[5] + (n - 5) * values[1]
     elif n == 9:
-        result = valors[1] + valors[10]
+        result = values[1] + values[10]
     elif n <= 30:
-        result = (n//10) * valors[10]
+        result = (n//10) * values[10]
     elif n == 40:
-        result = valors[10] + valors[50]
+        result = values[10] + values[50]
     elif n < 90:
-        result = valors[50] + ((n//10) - 5) * valors[10]
+        result = values[50] + (n//10 - 5) * values[10]
     elif n == 90:
-        result = valors[10] + valors[100]
+        result = values[10] + values[100]
+    elif n <= 300:
+        result = (n//100) * values[100]
+    elif n == 400:
+        result = values[100] + values[500]
+    elif n < 900:
+        result = values[500] + (n//100 - 5) * values[100]
+    elif n == 900:
+        result = values[100] + values[1000]
+    elif n <= 3000:
+        result = n//1000*values[1000]
 
     return result
+
+
+def dividir_en_digitos(n: int):
+    lista_nums = []
+    cad = f'{n:04d}'
+    mult = 1000
+
+    for digito in cad:
+        lista_nums.append(int(digito) * mult)
+        mult /= 10
+
+    return lista_nums
+
+
+def digits_to_roman(lista_nums: list):
+    result = ''
+    for num in lista_nums:
+        result += to_roman(num)
+
+    return result
+
+
+def arabic_to_roman(n: int):
+    lista = dividir_en_digitos(n)
+    return digits_to_roman(lista)
--- test_roman_number.py
+++ test_roman_number.py
@@ -1,4 +1,4 @@
-from roman_funcs import to_roman
+from roman_funcs import to_roman, dividir_en_digitos, arabic_to_roman
 
 
 def test_romanos_simples():
@@ -33,3 +33,31 @@ def test_romanos_10_al_90():
     assert to_roman(70) == 'LXX'
     assert to_roman(80) == 'LXXX'
     assert to_roman(90) == 'XC'
+
+
+def test_romanos_100_al_1000():
+    assert to_roman(100) == 'C'
+    assert to_roman(200) == 'CC'
+    assert to_roman(300) == 'CCC'
+    assert to_roman(400) == 'CD'
+    assert to_roman(500) == 'D'
+    assert to_roman(600) == 'DC'
+    assert to_roman(700) == 'DCC'
+    assert to_roman(800) == 'DCCC'
+    assert to_roman(900) == 'CM'
+    assert to_roman(1000) == 'M'
+
+
+def test_dividir_en_digitos():
+    assert dividir_en_digitos(33) == [0, 0, 30, 3]
+    assert dividir_en_digitos(100) == [0, 100, 0, 0]
+    assert dividir_en_digitos(2024) == [2000, 0, 20, 4]
+
+
+def dconversion():
+    assert arabic_to_roman(970) == 'MCMLXX'
+    assert arabic_to_roman(1999) == 'MCMXCIX'
+    assert arabic_to_roman(2024) == 'MMXXIV'
+    assert arabic_to_roman(33) == 'XXXIII'
+    assert arabic_to_roman(100) == 'C'
+    assert arabic_to_roman(3999) == 'MMMCMXCIX'
