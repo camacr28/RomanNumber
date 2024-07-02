26f1d394c61ac7ef52316c2b9b5cf9e904298267
Carlos Mata <camacr28@gmail.com>
2024-07-02 19:03:33 +0200

    Test 10-90

--- roman_funcs.py
+++ roman_funcs.py
@@ -11,7 +11,9 @@ valors = {
 
 def to_roman(n):
 
-    if n <= 3:
+    if n in valors:
+        result = valors[n]
+    elif n <= 3:
         result = n * valors[1]
     elif n == 4:
         result = valors[1] + valors[5]
@@ -19,6 +21,14 @@ def to_roman(n):
         result = valors[5] + (n - 5) * valors[1]
     elif n == 9:
         result = valors[1] + valors[10]
+    elif n <= 30:
+        result = (n//10) * valors[10]
+    elif n == 40:
+        result = valors[10] + valors[50]
+    elif n < 90:
+        result = valors[50] + ((n//10) - 5) * valors[10]
+    elif n == 90:
+        result = valors[10] + valors[100]
     else:
         result = valors[n]
 
